6f53c2f037737eaa9369b806af8db220d2753daa
Kaningstan <kaningstan2328@gmail.com>
2023-12-07 15:59:04 +0530

    added20

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// require=dotenv library into your js code ,require =important modules,dotenv=is the modules that helps you load environment modules
+// config=this method  is called on the importanted dotenv module
 require('dotenv').config();
+// 
 
+// require('express')=important the express modules
+// The express module contains all the functionality and features of the Express web framework.
+// const express=declares a constant variable ,assigns the imported 'express' module to it.
+
+// mongoose=mongodb odm ()to translate the code and its representation from MongoDB to the Node. js server
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
